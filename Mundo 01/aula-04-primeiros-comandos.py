fc7ee24e53d7990e3d30c3c3864a001f241b8604
Gabriela Oliveira <gabrielaoliveirag105@gmail.com>
2026-08-10 20:55:07 -0300

    exercicios

--- Mundo 01/aula-04-primeiros-comandos.py
+++
@@ -1,6 +0,0 @@
-# Aula 04 - Primeiros Comandos
-print('Olá, mundo!')
-# O comando print('') é usado para exibir mensagens na tela.
-
-
-
--- Mundo 01/aula-04-variaveis-entrada-usuario.py
+++
@@ -1,8 +0,0 @@
-# Aula 04 - Interação com o usuário
-# O comando input() é usado para receber dados do usuário.
-# print - escreva/ input - leia
-
-nome = input('Qual é o seu nome? ')
-idade = input('Qual é a sua idade? ')
-peso = input('Qual é o seu peso? ')
-print('Olá', nome, '! Você tem', idade, 'anos e está pesando', peso, 'kg.')
